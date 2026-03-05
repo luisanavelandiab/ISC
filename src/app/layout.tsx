9d1806b8ca11b845a3b22644b0c878e98407f438
Luisana <luisanavelandiab@gmail.com>
2026-03-04 19:13:38 -0500

    remove force-dynamic from layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,3 @@
-export const dynamic = "force-dynamic";
 import { AuthProvider } from "@/context/AuthContext";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
