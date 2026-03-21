7b43d0eb0fbfe6defdab8cc2bd282558943d607e
Luisana <luisanavelandiab@gmail.com>
2026-03-21 16:45:50 -0500

    Integración OneSignal notificaciones push

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -6,28 +6,18 @@ module.exports = {
   },
   extends: [
     "eslint:recommended",
-    "plugin:import/errors",
-    "plugin:import/warnings",
-    "plugin:import/typescript",
-    "google",
     "plugin:@typescript-eslint/recommended",
   ],
   parser: "@typescript-eslint/parser",
   parserOptions: {
-    project: ["tsconfig.json", "tsconfig.dev.json"],
+    project: ["tsconfig.json"],
+    tsconfigRootDir: __dirname,
     sourceType: "module",
   },
-  ignorePatterns: [
-    "/lib/**/*", // Ignore built files.
-    "/generated/**/*", // Ignore generated files.
-  ],
-  plugins: [
-    "@typescript-eslint",
-    "import",
-  ],
+  ignorePatterns: ["/lib/**/*"],
+  plugins: ["@typescript-eslint"],
   rules: {
-    "quotes": ["error", "double"],
-    "import/no-unresolved": 0,
-    "indent": ["error", 2],
+    quotes: ["error", "double"],
+    indent: ["error", 2],
   },
 };
